3e4249e12ab8832a360374e0d1a2ff4e50bcdb50
yoonjoohye <67yjh00@gmail.com>
2017-05-23 20:56:54 +0900

    #3 call back

--- app/src/main/java/a67yjh00/com/example/lg/contextmenutest/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/contextmenutest/MainActivity.java
@@ -1,7 +1,14 @@
 package a67yjh00.com.example.lg.contextmenutest;
 
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but1=(Button) findViewById(R.id.but1);
+        Button but2=(Button)findViewById(R.id.but2);
+        registerForContextMenu(but1);
+        registerForContextMenu(but2);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater=getMenuInflater();
+        if(v.getId()==R.id.but1){
+            menu.setHeaderTitle("배경색 변경");
+            //menu.setHeaderIcon(R.drawable.icon1);
+            menuInflater.inflate(R.menu.menu1,menu);
+        }
+        if(v.getId()==R.id.but2)
+            menuInflater.inflate(R.menu.menu2,menu);
     }
 }
